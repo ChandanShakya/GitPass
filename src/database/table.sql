a304bbfb55f5faddcc8ff3020ff074259917c701
ZXY-CC-3ag13 <zxy-cc-3ag13@protonmail.com>
2022-03-24 05:26:31 +0545

    Add table structure and brand logo

---
+++ src/database/table.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE id18657476_gitpass;
+USE id18657476_gitpass;
+
+CREATE TABLE login(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE passwords(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    site VARCHAR(255) NOT NULL,
+    username VARCHAR(100),
+    password VARCHAR(100) NOT NULL,
+    favorite BOOLEAN DEFAULT FALSE,
+    user_id INT DEFAULT 1
+);
