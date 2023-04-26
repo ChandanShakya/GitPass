f2a67eda74b7c8596fda6c00a7c77a3ac6462f94
BCA20XX <BCA20XX@pm.me>
2023-04-26 02:56:11 -0400

    change to pdo

--- src/php/conn.php
+++ src/php/conn.php
@@ -1,11 +1,15 @@
-<?php 
-    $host = 'mysql-server';
-    $user = 'root';
-    $password = 'secret';
-    $database = 'id18657476_gitpass';
+<?php
+$host = 'mysql-server';
+$user = 'root';
+$password = 'secret';
+$database = 'id18657476_gitpass';
 
-    $conn = mysqli_connect($host, $user, $password, $database);
-    if($conn->connect_error) {
-        echo "An unknown error occurred.";
-    }
+$dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+try {
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "An unknown error occurred: " . $e->getMessage();
+}
 ?>
