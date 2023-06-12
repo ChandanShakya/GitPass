2ffbcdc128aebd427a3f695c20764f589ec34256
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-06-13 01:17:57 +0545

    fixed links

--- src/php/head.php
+++ src/php/head.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Icon -->
-    <link rel="icon" href="../img/brand.svg" type="image/x-icon">
+    <link rel="icon" href="img/brand.svg" type="image/x-icon">
     <title>Git Pass</title>
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/mdb.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mdb.min.css" />
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/24df122632.js" crossorigin="anonymous"></script>
 </head>
