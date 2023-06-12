7452e572935e5519e9e599837abbe75a077df0d4
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-06-13 01:11:47 +0545

    fix logout

--- src/logout.php
+++ src/logout.php
@@ -2,7 +2,7 @@
     session_start();
     require_once "php/conn.php";
 
-    if(isset($_SESSION['unique_id']) && isset($_GET['id']) && $_GET['id'] === $_SESSION['unique_id']) {
+    if(isset($_SESSION['unique_id']) && isset($_GET['id']) && $_GET['id'] == $_SESSION['unique_id']) {
         $id = $_GET['id'];
         session_unset();
         session_destroy();
